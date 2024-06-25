4620c6e16ef8de8838eda685c4f8cad358596402
kemchuoihot <thinhpx33@gmail.com>
2024-06-25 16:01:17 +0700

    test

--- client/src/components/Employee/index.jsx
+++ client/src/components/Employee/index.jsx
@@ -51,17 +51,17 @@ const Employee = () => {
         // const navigate = useNavigate();
         setLoading(true);
         event.preventDefault();
-        axios.post("https://pos-server-beige.vercel.app/account", {name, email})
-        .then(response => {
-            setLoading(false);
-            // navigate("/dashboard/employee");
-            console.log(response); // Now this will correctly log the Axios response
-        })
-        .catch(error => {
-            setLoading(false);
-            console.error(error);
-            setError(error.response?.data?.message || "An error occurred");
-        });
+        axios.post("https://pos-server-beige.vercel.app/account", { email }, {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          })
+          .then(response => {
+            console.log(response);
+          })
+          .catch(error => {
+            console.error("Error making the request:", error.response.data);
+          });
     }
 
     const handleRefresh = () => {
