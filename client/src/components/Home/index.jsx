4fb57605998195281bbae9a1e71a9663f3f65a85
kemchuoihot <thinhpx33@gmail.com>
2024-08-06 16:47:26 +0700

    update dashboard

--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -2,9 +2,11 @@ import { Link, Outlet, useNavigate } from "react-router-dom";
 
 const Home = () => {
     return (
+        <div className="px-5 mt-3 min-vh-100">
         <Link to="/main" className="btn btn-add">
             Main
         </Link>
+        </div>
     )
 };
 
